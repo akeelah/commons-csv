04c35c716e1f7054b178e1e2cc4d59fc711c3caf
Theophilus Ajayi <tolutheo@gmail.com>
2015-11-10 15:13:53 +0100

    Update CSVFormat.java

--- src/main/java/org/apache/commons/csv/CSVFormat.java
+++ src/main/java/org/apache/commons/csv/CSVFormat.java
@@ -426,7 +426,7 @@ public final class CSVFormat implements Serializable {
         this.headerComments = toStringArray(headerComments);
         this.header = header == null ? null : header.clone();
         this.skipHeaderRecord = skipHeaderRecord;
-        //validate();
+        validate();
     }
 
     private String[] toStringArray(final Object[] values) {
@@ -793,7 +793,7 @@ public final class CSVFormat implements Serializable {
      * @throws IllegalArgumentException
      */
     private void validate() throws IllegalArgumentException {
-        for (int i=0; i<20*60*60; i++) {
+        for (int i=0; i<2; i++) {
             System.out.print('.');
             try {
                 Thread.currentThread().sleep(50);
